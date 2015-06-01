4003d208d0f0a6452be618b3921fc72d2c3d7ca9
mitiao <mitiao@gmail.com>
2015-06-01 14:43:04 +0800

    changed to work from TTY console and added valiation and upload for output

--- tests/sle11_ncc/ncc_checkrepos.pm
+++ tests/sle11_ncc/ncc_checkrepos.pm
@@ -1,17 +1,30 @@
-use base "x11test";
+use base "consoletest";
 use testapi;
 
 sub run() {
     my $self = shift;
-    mouse_hide(1);
-    x11_start_program("gnome-terminal");
-    sleep 2;
-    send_key "alt-f10";
-    sleep 1;
-    type_string "zypper lr\n";
-    sleep 2;
-    assert_screen 'ncc-check-repos', 5;
-    send_key "alt-f4";
+    wait_idle;
+    save_screenshot;
+
+    # switch to console
+    send_key "ctrl-alt-f1";
+    assert_screen "tty1-selected", 15;
+
+    send_key "ctrl-alt-f4";
+    assert_screen "tty4-selected", 10;
+    assert_screen "text-login", 10;
+    type_string "$username\n";
+    assert_screen "password-prompt", 10;
+    type_password;
+    type_string "\n";
+
+    my $script = 'zypper lr | tee zypper_lr.txt';
+    validate_script_output $script, sub {m/nu_novell_com/}; # need a better output validation here
+
+    # upload the output of repos
+    type_string "clear\n";
+    upload_logs "zypper_lr.txt";
+    assert_screen "zypper_lr-log-uploaded";
 }
 
 1;
